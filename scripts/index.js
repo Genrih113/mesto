704405ec5402b4c978a6d46cedd168bffeee7c1b
Gennadij Tolstov <genrih113@yandex.ru>
2020-10-18 00:26:02 +0300

    сглупить и исправить конфликты уже потом- ручками в файлах

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-let personEditButton = document.querySelector('.person__edit-button');
-let popup = document.querySelector('.popup');
-let popupCloseButton = document.querySelector('.popup__close');
-let popupName = document.querySelector('.popup__name');
-let popupPassion = document.querySelector('.popup__passion');
-let personName = document.querySelector('.person__name');
-let personPassion = document.querySelector('.person__passion');
-let popupForm = document.querySelector('.popup__container');
-
-function popupToggleFn() {
-  if (popup.classList.contains('popup_opened') === false) {
-    personInfoCopyToPopupFn();
-  }
-  popup.classList.toggle('popup_opened');
-};
-
-function personInfoCopyToPopupFn() {
-  popupName.value = personName.textContent;
-  popupPassion.value = personPassion.textContent;
-};
-
-function personInfoCopyToPageFn(event) {
-  event.preventDefault();
-  personName.textContent = popupName.value;
-  personPassion.textContent = popupPassion.value;
-  popupToggleFn();
-};
-
-function popupBackgrClickCloseFn(event) {
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-  popupToggleFn();
-};
-
-personEditButton.addEventListener('click', popupToggleFn);
-popupCloseButton.addEventListener('click', popupToggleFn);
-popupForm.addEventListener('submit', personInfoCopyToPageFn);
-popup.addEventListener('click', popupBackgrClickCloseFn);
-=======
 //переменные отображаемые на странице
 const personEditButton = document.querySelector('.person__edit-button');
 const personName = document.querySelector('.person__name');
@@ -201,4 +160,3 @@ placeViewPopup.addEventListener('click', () => {
   }
   togglePopup(placeViewPopup);
 });
->>>>>>> develop
